a0f85a7ae0ff1598d44c8326321e32deef40750f
Udaloiii <udaloiiisohappy@gmail.com>
2024-03-17 19:00:52 +0300

    changed style

--- src/assets/logo/arrow-down.tsx
+++ src/assets/logo/arrow-down.tsx
@@ -13,7 +13,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
         d={
           'M5.514 9.458a1 1 0 0 1 1.64-.77l5.36 4.48 5.37-4.32a1 1 0 0 1 1.41.15 1 1 0 0 1-.15 1.46l-6 4.83a1 1 0 0 1-1.27 0l-6-5a1 1 0 0 1-.36-.83Z'
         }
-        fill={'#000'}
+        fill={'currentColor'}
       />
     </g>
     <defs>

e3e9109fc3d2c0d46b3fd4524f622329c0d8403d
Udaloiii <udaloiiisohappy@gmail.com>
2024-03-26 21:00:10 +0300

    added pagination (tsx, module, stories) & hook usePagination

---
+++ src/components/ui/pagination/pagination.stories.tsx
@@ -0,0 +1,22 @@
+import { Pagination } from '@/components/ui/pagination/pagination'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  argTypes: {},
+  component: Pagination,
+  tags: ['autodocs'],
+  title: 'Components/Pagination',
+} satisfies Meta<typeof Pagination>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const PaginationExample: Story = {
+  args: {
+    currentPage: 1,
+    onPageChange: () => {},
+    pageSize: 3,
+    siblingCount: 2,
+    totalCount: 50,
+  },
+}

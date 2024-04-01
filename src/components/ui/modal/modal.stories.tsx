ecb498504e61bd60a6b0af4c030a77ac17051fe5
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-01 21:23:56 +0300

    added props; changed style; refactored

--- src/components/ui/modal/modal.stories.tsx
+++ src/components/ui/modal/modal.stories.tsx
@@ -1,5 +1,10 @@
+import Image from '@/assets/logo/image'
+import { Button } from '@/components/ui/button'
 import { Modal } from '@/components/ui/modal/modal'
+import { Typography } from '@/components/ui/typography'
 import { Meta, StoryObj } from '@storybook/react'
+
+import icon from '../../../assets/img/user-avatar.webp'
 
 const meta = {
   argTypes: {},
@@ -13,6 +18,36 @@ type Story = StoryObj<typeof meta>
 
 export const ModalExample: Story = {
   args: {
+    children:
+      'asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd ',
+    showCloseBtn: true,
     title: 'Create new pack',
+    titlePrimary: 'Title primary',
+    titleSecondary: 'Title secondary',
+  },
+}
+
+export const ModalCreateNewPack: Story = {
+  args: {
+    children: (
+      <>
+        <img alt={'icon'} src={icon} />
+        <p>
+          asdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd
+          asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd
+          asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd
+          asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd
+          asdasdasdasd asd asd asd asd asd asd asd asd asdasdasdasd asd asd asd asd asd asd asd asd
+        </p>
+        <Button fullWidth variant={'secondary'}>
+          <Image />
+          <Typography variant={'subtitle2'}>Change cover</Typography>
+        </Button>
+      </>
+    ),
+    showCloseBtn: true,
+    title: 'Create new pack',
+    titlePrimary: 'Title primary',
+    titleSecondary: 'Title secondary',
   },
 }
--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,14 +1,26 @@
-import Close from '@/assets/logo/close'
+import { ComponentProps } from 'react'
+
 import { Button } from '@/components/ui/button'
+import { ModalFooter } from '@/components/ui/modal/modal-footer/modal-footer'
+import { ModalHeader } from '@/components/ui/modal/modal-header/modal-header'
 import { Typography } from '@/components/ui/typography'
 import * as RadixModal from '@radix-ui/react-dialog'
 
 import s from './modal.module.scss'
 
 type ModalProps = {
+  showCloseBtn?: boolean
   title?: string
-}
-export const Modal = ({ title }: ModalProps) => (
+  titlePrimary?: string
+  titleSecondary?: string
+} & ComponentProps<'div'>
+export const Modal = ({
+  children,
+  showCloseBtn,
+  title,
+  titlePrimary,
+  titleSecondary,
+}: ModalProps) => (
   <RadixModal.Root>
     <RadixModal.Trigger asChild>
       <Button variant={'primary'}>Edit profile</Button>
@@ -16,16 +28,15 @@ export const Modal = ({ title }: ModalProps) => (
     <RadixModal.Portal>
       <RadixModal.Overlay className={s.dialogOverlay} />
       <RadixModal.Content className={s.dialogContent}>
-        <RadixModal.Title className={s.DialogTitle}>{title}</RadixModal.Title>
-        <RadixModal.Description className={'DialogDescription'}>
-          <Typography variant={'body1'}>{title}</Typography>
-        </RadixModal.Description>
-
-        <RadixModal.Close asChild>
-          <Button className={s.close} variant={'icon'}>
-            <Close />
-          </Button>
-        </RadixModal.Close>
+        <RadixModal.Title className={s.DialogTitle}>
+          <ModalHeader showCloseBtn={showCloseBtn} title={title} />
+        </RadixModal.Title>
+        <div className={s.mainContent}>
+          <Typography className={s.content} variant={'body1'}>
+            {children}
+          </Typography>
+        </div>
+        <ModalFooter titlePrimary={titlePrimary} titleSecondary={titleSecondary} />
       </RadixModal.Content>
     </RadixModal.Portal>
   </RadixModal.Root>

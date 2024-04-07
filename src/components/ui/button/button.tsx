372d4c063e174e38391c1f09100577f46aab2a91
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-07 13:53:04 +0300

    added in props onSort for sort in table

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -5,12 +5,14 @@ import s from './button.module.scss'
 type ButtonProps = {
   children?: ReactNode
   fullWidth?: boolean
+  onSort?: (value: string) => void
   variant?: 'icon' | 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<'button'>
 export const Button = ({
   children,
   className,
   fullWidth,
+  onSort,
   variant = 'primary',
   ...rest
 }: ButtonProps) => {

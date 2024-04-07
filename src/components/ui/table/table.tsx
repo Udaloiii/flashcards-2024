d2501e0cc3bacff6aa3794ff04ac516fa569854a
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-07 13:46:36 +0300

    added sortByColumn for to highlight a column and display an icon; added sortDirection to flip the icon when pressed again and sort accordingly; added handleSort for change sortByColumn & sortDirection; added sortedItems for visibility in page current sort items

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import img from '@/assets/img/user-avatar.webp'
 import { ControlItems } from '@/components/ui/table/control-items/control-items'
 import { Stars } from '@/components/ui/table/stars/stars'
@@ -9,21 +11,54 @@ type TableProps = {
   items?: string[] // пока так, потом посмотреть что с бека приходит
   myCards?: boolean
 }
-export const Table = ({ items, myCards }: TableProps) => {
+export const Table = ({ items = [], myCards }: TableProps) => {
+  const [sortByColumn, setSortByColumn] = useState<string>('')
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc')
+
+  const handleSort = (columnName: string) => {
+    if (sortByColumn === columnName) {
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')
+    } else {
+      setSortByColumn(columnName)
+      setSortDirection('asc')
+    }
+  }
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortByColumn === 'Name') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    } else if (sortByColumn === 'Last update') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    }
+
+    return 0
+  })
+
   return (
     <table className={s.container}>
       <thead>
         <tr>
-          <TableItem as={'th'} title={'Name'} />
+          <TableItem
+            as={'th'}
+            onClick={() => handleSort('Name')}
+            sortBy={sortByColumn}
+            sortDirection={sortDirection}
+            title={'Name'}
+          />
           <TableItem as={'th'} title={'Cards'} />
-          <TableItem as={'th'} title={'Last update'} />
+          <TableItem
+            as={'th'}
+            onClick={() => handleSort('Last update')}
+            sortBy={sortByColumn}
+            sortDirection={sortDirection}
+            title={'Last update'}
+          />
           <TableItem as={'th'} title={'Author'} />
           <th></th>
           <th></th>
         </tr>
       </thead>
       <tbody>
-        {items?.map((el, i: number) => (
+        {sortedItems?.map((el, i: number) => (
           <tr key={i}>
             <TableItem as={'td'} className={s.big} image={img} title={el} />
             <TableItem as={'td'} title={el} />

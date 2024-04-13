60e75167b4e947208382ded4a9169a038880f02f
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-13 19:16:28 +0300

    refactored

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,74 +1,45 @@
-import { useState } from 'react'
-
-import img from '@/assets/img/user-avatar.webp'
-import { ControlItems } from '@/components/ui/table/control-items/control-items'
-import { Stars } from '@/components/ui/table/stars/stars'
-import { TableItem } from '@/components/ui/table/table-item/table-item'
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
 
 import s from './table.module.scss'
 
 type TableProps = {
-  items?: string[] // пока так, потом посмотреть что с бека приходит
-  myCards?: boolean
-}
-export const Table = ({ items = [], myCards }: TableProps) => {
-  const [sortByColumn, setSortByColumn] = useState<string>('')
-  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc')
-
-  const handleSort = (columnName: string) => {
-    if (sortByColumn === columnName) {
-      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')
-    } else {
-      setSortByColumn(columnName)
-      setSortDirection('asc')
-    }
+  children: ReactNode
+  className?: string
+} & ComponentPropsWithoutRef<'table'>
+export const Table = forwardRef<ElementRef<'table'>, TableProps>(
+  ({ children, className, ...rest }, ref) => {
+    return (
+      <table className={`${s.table} ${className}`} ref={ref} {...rest}>
+        {children}
+      </table>
+    )
   }
-  const sortedItems = [...items].sort((a, b) => {
-    if (sortByColumn === 'Name') {
-      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
-    } else if (sortByColumn === 'Last update') {
-      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
-    }
+)
+export const TableHeader = forwardRef<ElementRef<'thead'>, ComponentPropsWithoutRef<'thead'>>(
+  ({ ...rest }, ref) => {
+    return <thead ref={ref} {...rest}></thead>
+  }
+)
+export const TableRow = forwardRef<ElementRef<'tr'>, ComponentPropsWithoutRef<'tr'>>(
+  ({ ...rest }, ref) => {
+    return <tr ref={ref} {...rest} />
+  }
+)
 
-    return 0
-  })
+export const TableHead = forwardRef<ElementRef<'th'>, ComponentPropsWithoutRef<'th'>>(
+  ({ ...rest }, ref) => {
+    return <th ref={ref} {...rest} />
+  }
+)
 
-  return (
-    <table className={s.container}>
-      <thead>
-        <tr>
-          <TableItem
-            as={'th'}
-            onClick={() => handleSort('Name')}
-            sortByColumn={sortByColumn}
-            sortDirection={sortDirection}
-            title={'Name'}
-          />
-          <TableItem as={'th'} title={'Cards'} />
-          <TableItem
-            as={'th'}
-            onClick={() => handleSort('Last update')}
-            sortByColumn={sortByColumn}
-            sortDirection={sortDirection}
-            title={'Last update'}
-          />
-          <TableItem as={'th'} title={'Author'} />
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedItems?.map((el, i: number) => (
-          <tr key={i}>
-            <TableItem as={'td'} className={s.big} image={img} title={el} />
-            <TableItem as={'td'} title={el} />
-            <TableItem as={'td'} title={el} />
-            <TableItem as={'td'} title={el} />
-            <Stars />
-            <ControlItems myCards={myCards} />
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
-}
+export const TableBody = forwardRef<ElementRef<'tbody'>, ComponentPropsWithoutRef<'tbody'>>(
+  ({ ...rest }, ref) => {
+    return <tbody ref={ref} {...rest} />
+  }
+)
+
+export const TableCell = forwardRef<ElementRef<'td'>, ComponentPropsWithoutRef<'td'>>(
+  ({ ...rest }, ref) => {
+    return <td ref={ref} {...rest} />
+  }
+)

304e82f1e4e5e7d5120efe37e252b063b5a740de
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-13 19:19:54 +0300

    added function for sort; added property for sort-icon;

--- src/components/ui/table/decks-table/decks-table-head/decks-table-head.tsx
+++ src/components/ui/table/decks-table/decks-table-head/decks-table-head.tsx
@@ -1,17 +1,51 @@
+import ArrowDown from '@/assets/logo/arrow-down'
+import { Button } from '@/components/ui/button'
 import { TableHead, TableRow } from '@/components/ui/table/table'
 import { Typography } from '@/components/ui/typography'
 
-export const DecksTableHead = () => {
+import s from './decks-table-head.module.scss'
+
+type DecksTableHeadProps = {
+  onclick?: (value: string) => void
+  sortByColumn?: string
+  sortDirection?: 'asc' | 'desc'
+}
+export const DecksTableHead = ({ onclick, sortByColumn, sortDirection }: DecksTableHeadProps) => {
+  const sortIconCondition = sortDirection === 'desc' && s.sortUp
+
   return (
-    <TableRow>
-      <TableHead>
+    <TableRow className={s.container}>
+      <TableHead className={s.tableHead} onClick={() => onclick?.('Name')}>
         <Typography variant={'subtitle2'}>Name</Typography>
+        {sortByColumn === 'Name' && (
+          <Button
+            className={`${s.sort} ${sortIconCondition}`}
+            onSort={() => onclick?.('Name')}
+            variant={'icon'}
+          >
+            <ArrowDown />
+          </Button>
+        )}
       </TableHead>
-      <TableHead>
-        <Typography variant={'subtitle2'}>Cards</Typography>
+      <TableHead className={s.tableHead} onClick={() => onclick?.('Cards')}>
+        <Typography variant={'subtitle2'}>
+          Cards
+          {sortByColumn === 'Cards' && (
+            <Button className={`${s.sort} ${sortIconCondition}`} variant={'icon'}>
+              <ArrowDown />
+            </Button>
+          )}
+        </Typography>
       </TableHead>
-      <TableHead>
-        <Typography variant={'subtitle2'}>Last updated</Typography>
+      <TableHead className={s.tableHead} onClick={() => onclick?.('Last updated')}>
+        <Typography variant={'subtitle2'}>
+          Last updated
+          {sortByColumn === 'Last updated' && (
+            <Button className={`${s.sort} ${sortIconCondition}`} variant={'icon'}>
+              <ArrowDown />
+            </Button>
+          )}
+        </Typography>
       </TableHead>
       <TableHead>
         <Typography variant={'subtitle2'}>Created by</Typography>
--- src/components/ui/table/decks-table/decks-table.tsx
+++ src/components/ui/table/decks-table/decks-table.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { DecksTableBody } from '@/components/ui/table/decks-table/decks-table-body/decks-table-body'
 import { DecksTableHead } from '@/components/ui/table/decks-table/decks-table-head/decks-table-head'
 import { Table, TableBody, TableHeader } from '@/components/ui/table/table'
@@ -9,13 +11,40 @@ type DecksTableProps = {
   myCards?: boolean
 }
 export const DecksTable = ({ items, myCards }: DecksTableProps) => {
+  const [sortByColumn, setSortByColumn] = useState<string>('')
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc')
+
+  const handleSort = (columnName: string) => {
+    if (sortByColumn === columnName) {
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')
+    } else {
+      setSortByColumn(columnName)
+      setSortDirection('asc')
+    }
+  }
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortByColumn === 'Name') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    } else if (sortByColumn === 'Last updated') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    } else if (sortByColumn === 'Cards') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    }
+
+    return 0
+  })
+
   return (
     <Table className={s.container}>
       <TableHeader>
-        <DecksTableHead />
+        <DecksTableHead
+          onclick={handleSort}
+          sortByColumn={sortByColumn}
+          sortDirection={sortDirection}
+        />
       </TableHeader>
       <TableBody>
-        <DecksTableBody items={items} myCards={myCards} />
+        <DecksTableBody items={sortedItems} myCards={myCards} />
       </TableBody>
     </Table>
   )

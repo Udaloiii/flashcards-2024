6ea1815ad4e537efe2144f84f12692f422cbc888
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-15 22:07:17 +0300

    changed style

--- src/components/ui/table/decks-table/decks-table-body/decks-table-body.tsx
+++ src/components/ui/table/decks-table/decks-table-body/decks-table-body.tsx
@@ -2,6 +2,8 @@ import { ControlItems } from '@/components/ui/table/control-items/control-items'
 import { TableCell, TableRow } from '@/components/ui/table/table'
 import { Typography } from '@/components/ui/typography'
 
+import s from './decks-table-body.module.scss'
+
 type DecksTableBodyProps = {
   items: string[]
   myCards?: boolean
@@ -24,7 +26,7 @@ export const DecksTableBody = ({ items, myCards }: DecksTableBodyProps) => {
             <Typography variant={'body2'}>{el}</Typography>
           </TableCell>
           <TableCell>
-            <ControlItems myCards={myCards} />
+            <ControlItems className={s.controlItems} myCards={myCards} />
           </TableCell>
         </TableRow>
       ))}

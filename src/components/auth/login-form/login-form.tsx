7a253165d0777e01ae640c71c7ac8f72cac8ab8d
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-17 19:59:57 +0300

    create login-form; added styles; moved validation-schema in .ts

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,25 +1,69 @@
 import { useForm } from 'react-hook-form'
 
-import { Button } from '../../ui/button'
-import { Textfield } from '../../ui/textfield'
+import { loginSchema } from '@/components/auth/login-form/login-schema'
+import { ControlledCheckbox } from '@/components/controlled/controlled-checkbox'
+import { ControlledTextfield } from '@/components/controlled/controlled-textfield'
+import { Card } from '@/components/ui/card'
+import { Typography } from '@/components/ui/typography'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
-type FormValues = {
-  email: string
-  password: string
-}
+import s from './login-form.module.scss'
+
+import { Button } from '../../ui/button'
+
+type FormValues = z.infer<typeof loginSchema> // тяну тип сразу из loginSchema
 
 export const LoginForm = () => {
-  const { handleSubmit, register } = useForm<FormValues>()
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormValues>({
+    resolver: zodResolver(loginSchema),
+  })
 
   const onSubmit = (data: FormValues) => {
     console.log(data)
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Textfield {...register('email')} label={'email'} />
-      <Textfield {...register('password')} label={'password'} variant={'password'} />
-      <Button type={'submit'}>Submit</Button>
-    </form>
+    <Card className={s.wrap}>
+      <Typography as={'h1'} className={s.title} variant={'h1'}>
+        Sign in
+      </Typography>
+      <form className={s.container} onSubmit={handleSubmit(onSubmit)}>
+        <ControlledTextfield
+          control={control}
+          error={errors.email?.message}
+          label={'Email'}
+          name={'email'}
+        />
+        <ControlledTextfield
+          control={control}
+          error={errors.password?.message}
+          label={'Password'}
+          name={'password'}
+          variant={'password'}
+        />
+        <ControlledCheckbox control={control} label={'remember me'} name={'rememberMe'} />
+        <div className={s.forgotButton}>
+          <Typography as={'a'} variant={'body2'}>
+            Forgot password?
+          </Typography>
+        </div>
+        <Button fullWidth type={'submit'}>
+          Sign In
+        </Button>
+      </form>
+      <div className={s.createAcc}>
+        <Typography className={s.haveAcc} variant={'body2'}>
+          Don`t have an account?
+        </Typography>
+        <Typography as={'a'} variant={'subtitle1'}>
+          Sign Up
+        </Typography>
+      </div>
+    </Card>
   )
 }

daa72c328602828ccff484914f4e7a3efcbd0dd3
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-28 23:47:10 +0300

    refactored edit-profile; changed style

--- src/pages/edit-profile/edit-profile.tsx
+++ src/pages/edit-profile/edit-profile.tsx
@@ -1,18 +1,39 @@
+import { ChangeEvent, useRef } from 'react'
+
 import Edit from '@/assets/logo/edit'
 import LogOut from '@/assets/logo/log-out'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import { EditableSpan } from '@/components/ui/editable-span/editable-span'
+import { EditableText } from '@/components/ui/editable-span/editable-text'
+import { useEditableSpan } from '@/components/ui/editable-span/hook/useEditableSpan'
 import { Typography } from '@/components/ui/typography'
 
 import s from './edit-profile.module.scss'
 
 type EditProfileProps = {
-  email?: string
+  email: string
   icon?: string
   name: string
 }
 export const EditProfile = ({ email, icon, name }: EditProfileProps) => {
+  const { changeOn, setChangeOn, toggleMode } = useEditableSpan()
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click()
+    }
+  }
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0]
+
+    if (file) {
+      // Обработка выбранного файла, пока заглушка
+      console.log('Выбран файл:', file)
+    }
+  }
+
   return (
     <Card className={s.container}>
       <Typography as={'h1'} variant={'h1'}>
@@ -20,20 +41,39 @@ export const EditProfile = ({ email, icon, name }: EditProfileProps) => {
       </Typography>
       <div className={s.avatarBlock}>
         <img alt={'user'} className={s.avatar} src={icon} />
-        <Button variant={'icon'}>
+        <Button onClick={handleButtonClick} variant={'icon'}>
           <Edit />
         </Button>
+        <input
+          accept={'image/*'}
+          onChange={handleFileChange}
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          type={'file'}
+        />
       </div>
+
       <Typography as={'h2'} className={s.name} variant={'h2'}>
-        <EditableSpan title={name} trigger={<Edit />} />
+        <EditableText
+          changeOn={changeOn}
+          label={'Nickname'}
+          setChangeOn={setChangeOn}
+          title={name}
+          toggleMode={toggleMode}
+          trigger={<Edit />}
+        />
       </Typography>
-      <Typography className={s.email} variant={'body2'}>
-        {email}
-      </Typography>
-      <Button variant={'secondary'}>
-        <LogOut />
-        <Typography variant={'subtitle2'}>Logout</Typography>
-      </Button>
+      {!changeOn && (
+        <>
+          <Typography className={s.email} variant={'body2'}>
+            {email}
+          </Typography>
+          <Button variant={'secondary'}>
+            <LogOut />
+            <Typography variant={'subtitle2'}>Logout</Typography>
+          </Button>
+        </>
+      )}
     </Card>
   )
 }

f61a7055d760536b253ff01e67beba1e8a857b8e
Udaloiii <udaloiiisohappy@gmail.com>
2024-04-29 18:21:49 +0300

    changed page.tsx; added style for this

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -1,3 +1,8 @@
-export const Page = () => {
-  return <div></div>
+import { ComponentPropsWithoutRef } from 'react'
+
+import s from './page.module.scss'
+
+type PageProps = ComponentPropsWithoutRef<'div'>
+export const Page = ({ className, ...rest }: PageProps) => {
+  return <div className={`${s.page} ${className}`} {...rest}></div>
 }

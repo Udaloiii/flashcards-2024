a3da2c7eba69eefa4e74f38ce1f799aca5cbb8f1
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-01 14:05:16 +0300

    refactored with use item-table-head;

--- src/components/ui/table/cards-table/cards-table.tsx
+++ src/components/ui/table/cards-table/cards-table.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
+
 import { CardsTableBody } from '@/components/ui/table/cards-table/cards-table-body/cards-table-body'
-import { CardsTableHead } from '@/components/ui/table/cards-table/cards-table-head/cards-table-head'
+import { ItemTableHead } from '@/components/ui/table/item-table-head/item-table-head'
 import { Table, TableBody, TableHeader } from '@/components/ui/table/table'
 
 import s from './cards-table.module.scss'
@@ -8,13 +10,44 @@ type CardsTableProps = {
   items: string[]
 }
 export const CardsTable = ({ items }: CardsTableProps) => {
+  const [sortByColumn, setSortByColumn] = useState<string>('')
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc')
+
+  const handleSort = (columnName: string) => {
+    if (sortByColumn === columnName) {
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')
+    } else {
+      setSortByColumn(columnName)
+      setSortDirection('asc')
+    }
+  }
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortByColumn === 'Question') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    } else if (sortByColumn === 'Last Updated') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    } else if (sortByColumn === 'Answer') {
+      return sortDirection === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+    }
+
+    return 0
+  })
+
   return (
     <Table className={s.container}>
       <TableHeader>
-        <CardsTableHead />
+        {/*<CardsTableHead />*/}
+        <ItemTableHead
+          firstTitle={'Question'}
+          fourthTitle={'Grade'}
+          onclick={handleSort}
+          secondTitle={'Answer'}
+          sortByColumn={sortByColumn}
+          sortDirection={sortDirection}
+        />
       </TableHeader>
       <TableBody>
-        <CardsTableBody items={items} />
+        <CardsTableBody items={sortedItems} />
       </TableBody>
     </Table>
   )

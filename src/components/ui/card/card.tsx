6da6f38cbe4a5a5ccb734020efa35dd4201b29e5
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-08 22:54:42 +0300

    added ComponentPropsWithoutRef; added rest-props

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,12 +1,16 @@
-import { FC, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, FC, ReactNode } from 'react'
 
 import s from './card.module.scss'
 
 type CardProps = {
   children?: ReactNode
   className?: string
-}
+} & ComponentPropsWithoutRef<'div'>
 
-export const Card: FC<CardProps> = ({ children, className }: CardProps) => {
-  return <div className={`${s.container} ${className}`}>{children}</div>
+export const Card: FC<CardProps> = ({ children, className, ...rest }: CardProps) => {
+  return (
+    <div className={`${s.container} ${className}`} {...rest}>
+      {children}
+    </div>
+  )
 }

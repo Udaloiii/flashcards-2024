0f8b391fe78602a5b655729d2fa908bb9c89eac0
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-09 22:19:35 +0300

    created flashcards-type

---
+++ src/services/flashcards-type.ts
@@ -0,0 +1,29 @@
+export type Author = {
+  id: string
+  name: string
+}
+
+export type Deck = {
+  author: Author
+  cardsCount: number
+  cover?: null | string
+  created: string
+  id: string
+  isPrivate: boolean
+  name: string
+  updated: string
+  userId: string
+}
+
+export type Pagination = {
+  currentPage: number
+  itemsPerPage: number
+  totalItems: number
+  totalPages: number
+}
+
+export type GetDeckResponse = {
+  items: Deck[]
+  maxCardsCount: number
+  pagination: Pagination
+}

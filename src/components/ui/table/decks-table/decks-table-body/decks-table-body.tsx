3073d3fc6f3f4b921ba933e96c17a4f4d3cdd08c
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-11 14:07:45 +0300

    moved type from string[] to DeckType[]; changed name items for rendering; added condition for disable

--- src/components/ui/table/decks-table/decks-table-body/decks-table-body.tsx
+++ src/components/ui/table/decks-table/decks-table-body/decks-table-body.tsx
@@ -1,35 +1,44 @@
 import { ControlItems } from '@/components/ui/table/control-items/control-items'
 import { TableCell, TableRow } from '@/components/ui/table/table'
 import { Typography } from '@/components/ui/typography'
+import { DeckType } from '@/services/flashcards-type'
 
 import s from './decks-table-body.module.scss'
 
 type DecksTableBodyProps = {
-  items: string[]
+  items?: DeckType[]
   myCards?: boolean
 }
 export const DecksTableBody = ({ items, myCards }: DecksTableBodyProps) => {
   return (
     <>
-      {items?.map((el, ind) => (
-        <TableRow key={ind}>
-          <TableCell>
-            <Typography variant={'body2'}>{el}</Typography>
-          </TableCell>
-          <TableCell>
-            <Typography variant={'body2'}>{el}</Typography>
-          </TableCell>
-          <TableCell>
-            <Typography variant={'body2'}>{el}</Typography>
-          </TableCell>
-          <TableCell>
-            <Typography variant={'body2'}>{el}</Typography>
-          </TableCell>
-          <TableCell>
-            <ControlItems className={s.controlItems} myDecks={myCards} />
-          </TableCell>
-        </TableRow>
-      ))}
+      {items?.map((el, ind) => {
+        const date = new Date(el.updated)
+
+        return (
+          <TableRow key={ind}>
+            <TableCell>
+              <Typography variant={'body2'}>{el.name}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant={'body2'}>{el.cardsCount}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant={'body2'}>{date.toLocaleDateString()}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant={'body2'}>{el.author.name}</Typography>
+            </TableCell>
+            <TableCell>
+              <ControlItems
+                className={s.controlItems}
+                disabled={el.cardsCount < 1}
+                myDecks={myCards}
+              />
+            </TableCell>
+          </TableRow>
+        )
+      })}
     </>
   )
 }

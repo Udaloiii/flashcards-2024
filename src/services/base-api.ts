66c1693acf02681ebc221c592be83ea808da56f8
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-16 19:33:56 +0300

    feat: added tag 'Auth' in tagTypes; refactor: comment prepareHeaders (now use data from response)

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -4,11 +4,11 @@ export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.flashcards.andrii.es',
     credentials: 'include',
-    prepareHeaders: headers => {
-      headers.append('x-auth-skip', 'true')
-    },
+    // prepareHeaders: headers => {
+    //   headers.append('x-auth-skip', 'true')
+    // }, // убрал, потому что сделал реальную авторизацию
   }),
   endpoints: () => ({}),
   reducerPath: 'baseApi',
-  tagTypes: ['Deck'],
+  tagTypes: ['Deck', 'Auth'],
 })

e280d09af5daa2d294003e7be3de832879e4c5d0
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-17 18:33:18 +0300

    feat: added all auth-requests for injectEndpoints; export hooks

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { baseApi } from '@/services/base-api'
-import { LoginType, User } from '@/services/flashcards-type'
+import { LoginType, SignUpArg, User } from '@/services/flashcards-type'
 
 export const authService = baseApi.injectEndpoints({
   endpoints: build => {
@@ -13,11 +13,85 @@ export const authService = baseApi.injectEndpoints({
         },
       }),
       logIn: build.mutation<void, LoginType>({
-        query: body => {
+        invalidatesTags: ['Auth'],
+        query: arg => {
           return {
-            body: body,
+            body: arg,
             method: 'POST',
             url: '/v1/auth/login',
+          }
+        },
+      }),
+      logOut: build.mutation<void, void>({
+        invalidatesTags: ['Auth'],
+        query: () => {
+          return {
+            method: 'POST',
+            url: '/v1/auth/logout',
+          }
+        },
+      }),
+      recoverPassword: build.mutation<void, { email: string; html?: string; subject?: string }>({
+        query: arg => {
+          return {
+            body: arg,
+            method: 'POST',
+            url: `/v1/auth/recover-password`,
+          }
+        },
+      }),
+      refreshToken: build.mutation<void, void>({
+        invalidatesTags: ['Auth'],
+        query: () => {
+          return {
+            method: 'POST',
+            url: `/v2/auth/refresh-token`,
+          }
+        },
+      }),
+      resendEmail: build.mutation<void, { html?: string; subject?: string; userId: string }>({
+        query: arg => {
+          return {
+            body: arg,
+            method: 'POST',
+            url: '/v1/auth/resend-verification-email',
+          }
+        },
+      }),
+      resetPassword: build.mutation<void, { body: { password: string }; token: string }>({
+        query: arg => {
+          return {
+            body: arg.body,
+            method: 'POST',
+            url: `/v1/auth/reset-password/${arg.token}`,
+          }
+        },
+      }),
+      signUp: build.mutation<User, SignUpArg>({
+        query: arg => {
+          return {
+            body: arg,
+            method: 'POST',
+            url: `/v1/auth/sign-up`,
+          }
+        },
+      }),
+      updateUser: build.mutation<User, { avatar?: string; name: string }>({
+        invalidatesTags: ['Auth'],
+        query: arg => {
+          return {
+            body: arg,
+            method: 'POST',
+            url: `/v1/auth/me`,
+          }
+        },
+      }),
+      verifyEmail: build.mutation<void, { code: string }>({
+        query: arg => {
+          return {
+            body: arg,
+            method: 'POST',
+            url: `/v1/auth/verify-email`,
           }
         },
       }),
@@ -25,4 +99,15 @@ export const authService = baseApi.injectEndpoints({
   },
 })
 
-export const { useAuthMeQuery, useLogInMutation } = authService
+export const {
+  useAuthMeQuery,
+  useLogInMutation,
+  useLogOutMutation,
+  useRecoverPasswordMutation,
+  useRefreshTokenMutation,
+  useResendEmailMutation,
+  useResetPasswordMutation,
+  useSignUpMutation,
+  useUpdateUserMutation,
+  useVerifyEmailMutation,
+} = authService

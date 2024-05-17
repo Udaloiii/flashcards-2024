c9788da3b5a28e9bb1c94a21ad8c80213dbf27bb
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-17 18:34:19 +0300

    refactor: remove comments; added tag 'Card' for tagTypes

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -4,11 +4,8 @@ export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.flashcards.andrii.es',
     credentials: 'include',
-    // prepareHeaders: headers => {
-    //   headers.append('x-auth-skip', 'true')
-    // }, // убрал, потому что сделал реальную авторизацию
   }),
   endpoints: () => ({}),
   reducerPath: 'baseApi',
-  tagTypes: ['Deck', 'Auth'],
+  tagTypes: ['Deck', 'Auth', 'Card'],
 })

131db9fc025907f12f853c347fb20fd347c9f5c3
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-18 21:19:24 +0300

    feat: added props in Header for dropdown

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,15 +5,17 @@ import { Header } from '@/components/ui/header'
 import s from './layout.module.scss'
 
 type LayoutProps = ComponentPropsWithoutRef<'div'> & {
+  email?: string
+  icon?: string
   isLoggedIn?: boolean
   userName?: string
 }
 
 export const Layout = forwardRef<ElementRef<'div'>, LayoutProps>(
-  ({ children, className, isLoggedIn, userName, ...rest }, ref) => {
+  ({ children, className, email, icon, isLoggedIn, userName, ...rest }, ref) => {
     return (
       <div className={className} ref={ref} {...rest}>
-        <Header isLoggedIn={isLoggedIn} userName={userName} />
+        <Header email={email} icon={icon} isLoggedIn={isLoggedIn} userName={userName} />
         <main className={s.main}>{children}</main>
       </div>
     )

c0b495e1da24022d9d45ad615e7f4c9e539b37b8
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-18 21:20:04 +0300

    feat: added types for auth & cards requests & response

--- src/services/flashcards-type.ts
+++ src/services/flashcards-type.ts
@@ -1,3 +1,5 @@
+// deck-types
+
 export type Author = {
   id: string
   name: string
@@ -57,4 +59,56 @@ export type UpdateDeckArgs = {
   cover?: string
   isPrivate?: boolean
   name?: string
+}
+
+// auth-types
+export type User = {
+  avatar?: string
+  created: string
+  email: string
+  id: string
+  isEmailVerified: boolean
+  name: string
+  updated: string
+}
+
+export type LoginType = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
+
+export type SignUpArg = {
+  email: string
+  html?: string
+  name?: string
+  password: string
+  sendConfirmationEmail?: boolean
+  subject?: string
+}
+
+// CARDS
+export type GetCardResponse = {
+  answer: string
+  answerImg: string
+  answerVideo: string
+  created: string
+  deckId: string
+  grade: number
+  id: string
+  question: string
+  questionImg: string
+  questionVideo: string
+  shots: number
+  updated: string
+  userId: string
+}
+
+export type UpdateCardRequest = {
+  answer?: string
+  answerImg?: string
+  answerVideo?: string
+  question?: string
+  questionImg?: string
+  questionVideo?: string
 }

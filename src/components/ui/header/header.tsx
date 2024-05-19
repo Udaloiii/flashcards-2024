86d091a97fed55928ebf61fe2454507b1da08948
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-19 21:49:19 +0300

    feat: added props; added new dropdown; added logOut in onClick for button

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -4,22 +4,21 @@ import HeaderLogo from '@/assets/logo/header-logo'
 import LogOut from '@/assets/logo/log-out'
 import Person from '@/assets/logo/person'
 import { Button } from '@/components/ui/button'
-import { Dropdown } from '@/components/ui/dropdown'
+import { Dropdown, DropdownItem, DropdownSeparator } from '@/components/ui/dropdown/dropdown'
+import { UserInfo } from '@/components/ui/dropdown/user-info/user-info'
 import { Typography } from '@/components/ui/typography'
+import { useLogOutMutation } from '@/services/auth.service'
 
 import s from './header.module.scss'
 
-import icon from '../../../assets/img/user-avatar.webp'
-
 type HeaderProps = {
+  email?: string
+  icon?: string
   isLoggedIn?: boolean
   userName?: string
 }
-export const Header = ({ isLoggedIn, userName }: HeaderProps) => {
-  const items = [
-    { icon: <Person />, title: 'My Profile' },
-    { icon: <LogOut />, title: 'Sign Out' },
-  ]
+export const Header = ({ email, icon, isLoggedIn, userName }: HeaderProps) => {
+  const [logOut] = useLogOutMutation()
 
   return (
     <header className={s.main}>
@@ -30,7 +29,33 @@ export const Header = ({ isLoggedIn, userName }: HeaderProps) => {
         {isLoggedIn ? (
           <div className={s.userInfo}>
             <Typography variant={'subtitle1'}>{userName}</Typography>
-            <Dropdown iconTrigger={icon} items={items} />
+            <Dropdown
+              trigger={
+                <img
+                  alt={'user'}
+                  src={icon}
+                  style={{ borderRadius: '50%', height: '50px', width: '50px' }}
+                />
+              }
+            >
+              <UserInfo email={email} icon={icon} userName={userName} />
+              <DropdownSeparator />
+              <Link to={'/profile'}>
+                <DropdownItem className={s.dropdownItem}>
+                  <Button variant={'icon'}>
+                    <Person />
+                  </Button>
+                  <Typography variant={'caption'}>My Profile</Typography>
+                </DropdownItem>
+              </Link>
+              <DropdownSeparator />
+              <DropdownItem className={s.dropdownItem}>
+                <Button onClick={() => logOut()} variant={'icon'}>
+                  <LogOut />
+                  <Typography variant={'caption'}>Sign Out</Typography>
+                </Button>
+              </DropdownItem>
+            </Dropdown>
           </div>
         ) : (
           <Button variant={'secondary'}>

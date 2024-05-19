e50cc1e1e99036aa968f5d8311c9e811510b9152
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-19 21:50:34 +0300

    refactor: added viewbox; remove clipPath={'url(#a)'} for correct visible

--- src/assets/logo/log-out.tsx
+++ src/assets/logo/log-out.tsx
@@ -4,11 +4,12 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
     fill={'none'}
     height={24}
     ref={ref}
+    viewBox={'0 0 24 24'}
     width={24}
     xmlns={'http://www.w3.org/2000/svg'}
     {...props}
   >
-    <g clipPath={'url(#a)'} fill={'currentColor'}>
+    <g fill={'currentColor'}>
       <path
         d={
           'M7 6a1 1 0 0 0 0-2H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h2a1 1 0 0 0 0-2H6V6h1ZM20.82 11.42l-2.82-4a1 1 0 1 0-1.63 1.16L18.09 11H10a1 1 0 0 0 0 2h8l-1.8 2.4a1 1 0 0 0 1.6 1.2l3-4a1 1 0 0 0 .02-1.18Z'
--- src/assets/logo/person.tsx
+++ src/assets/logo/person.tsx
@@ -4,11 +4,12 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
     fill={'none'}
     height={24}
     ref={ref}
+    viewBox={'0 0 24 24'}
     width={24}
     xmlns={'http://www.w3.org/2000/svg'}
     {...props}
   >
-    <g clipPath={'url(#a)'} fill={'currentColor'}>
+    <g fill={'currentColor'}>
       <path
         d={
           'M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8Zm0-6a2 2 0 1 1 0 4 2 2 0 0 1 0-4ZM12 13a7 7 0 0 0-7 7 1 1 0 1 0 2 0 5 5 0 1 1 10 0 1 1 0 0 0 2 0 7 7 0 0 0-7-7Z'

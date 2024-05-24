3aaedd2d25c47f8f057c7ee77d084fc6213495cf
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-24 20:52:58 +0300

    feat: made the question and answer display no longer than 5 words; added a picture display to the question and answer

--- src/components/ui/table/cards-table/cards-table-body/cards-table-body.tsx
+++ src/components/ui/table/cards-table/cards-table-body/cards-table-body.tsx
@@ -12,18 +12,41 @@ type DecksTableBodyProps = {
   titleCard?: string
 }
 export const CardsTableBody = ({ items, myDeck, titleCard }: DecksTableBodyProps) => {
+  // const { id } = useParams() // чтобы достать id из url
+  // const { isLoading } = useGetDeckByIdQuery({ id: id ?? '' })
+  //
+  // if (isLoading) {
+  //   return <div style={{ display: 'flex', justifyContent: 'center' }}>LOADING...</div>
+  // }
+
   return (
     <>
       {items?.map((el, ind) => {
         const date = new Date(el.updated)
 
+        const sliceAnswer = el.answer
+          .split(' ')
+          .filter((_, ind) => ind <= 5)
+          .join(' ')
+
+        const sliceQuestion = el.question
+          .split(' ')
+          .filter((_, ind) => ind <= 5)
+          .join(' ')
+
         return (
           <TableRow key={ind}>
             <TableCell>
-              <Typography variant={'body2'}>{el.question}</Typography>
+              <Typography className={s.question} variant={'body2'}>
+                {el.questionImg && <img alt={'questionImg'} src={el.questionImg} />}
+                {sliceQuestion}
+              </Typography>
             </TableCell>
             <TableCell>
-              <Typography variant={'body2'}>{el.answer}</Typography>
+              <Typography className={s.answer} variant={'body2'}>
+                {el.answerImg && <img alt={'answerImg'} src={el.answerImg} />}
+                {sliceAnswer}
+              </Typography>
             </TableCell>
             <TableCell>
               <Typography variant={'body2'}>{date.toLocaleDateString()}</Typography>

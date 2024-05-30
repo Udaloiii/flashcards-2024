7b37af2986f0960d6a06592cb552f701d027321d
Udaloiii <udaloiiisohappy@gmail.com>
2024-05-30 22:50:21 +0300

    feat: added rtk-query requests for data (user; minMaxCards count; data for table); feat: added local states (cardsCount, currPage, pageSize, author); feat: changed handleSliderChange; added useEffect for get minMaxCards count

--- src/pages/decks-list/decks-list.tsx
+++ src/pages/decks-list/decks-list.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import Delete from '@/assets/logo/delete'
 import { Button } from '@/components/ui/button'
 import { Modal } from '@/components/ui/modal'
-import { AddNewCard } from '@/components/ui/modal/add-new-card/add-new-card'
+import { AddNewPack } from '@/components/ui/modal/add-new-pack/add-new-pack'
 import { Pagination } from '@/components/ui/pagination/pagination'
 import { Slider } from '@/components/ui/slider'
 import { TabSwitcher } from '@/components/ui/tab-switcher'
@@ -11,50 +11,47 @@ import { DecksTable } from '@/components/ui/table/decks-table/decks-table'
 import { Textfield } from '@/components/ui/textfield'
 import { Typography } from '@/components/ui/typography'
 import { useDebounce } from '@/hooks/useDebounce'
+import { useAuthMeQuery } from '@/services/auth.service'
 import { useGetDecksQuery, useGetMinMaxCardsQuery } from '@/services/decks.service'
 
 import s from './decks-list.module.scss'
 
-// type DecksListProps = {
-//   currentPage?: number // текущая страница
-//   items?: string[]
-//   myDecks?: boolean
-//   onPageChange?: (page: number) => void // функция обратного вызова, вызываемая с обновленным значением страницы при изменении страницы
-//   pageSize?: number //  максимальный объем данных, видимых на одной странице
-//   siblingCount?: number //представляет минимальное количество кнопок страницы, отображаемых с каждой стороны кнопки текущей страницы. По умолчанию 1
-//   totalCount?: number // общее количество данных, доступных из источника
-// }
 export const DecksList = () => {
+  const { data: user } = useAuthMeQuery()
+  const { data: minMaxCards, isLoading: cardsCountLoading } = useGetMinMaxCardsQuery()
+
+  const [cardsCount, setCardsCount] = useState<number[]>()
+
   const [currPage, setCurrPage] = useState(1)
   const [pageSize, setPageSize] = useState(10)
 
-  const { data: minMaxCards } = useGetMinMaxCardsQuery()
-  const [minValue, setMinValue] = useState(minMaxCards?.min)
-  const [maxValue, setMaxValue] = useState(minMaxCards?.max)
   const [author, setAuthor] = useState('')
 
-  const { data, error, isLoading } = useGetDecksQuery({
-    authorId: author,
-    currentPage: currPage,
-    itemsPerPage: pageSize,
-    maxCardsCount: Number(useDebounce(maxValue || 100, 500)),
-    minCardsCount: Number(useDebounce(minValue || 0, 500)),
-  })
+  const { data, error, isLoading } = useGetDecksQuery(
+    {
+      authorId: author,
+      currentPage: currPage,
+      itemsPerPage: pageSize,
+      maxCardsCount: Number(useDebounce(cardsCount?.[1] ?? 0, 1000)),
+      minCardsCount: Number(useDebounce(cardsCount?.[0] ?? 100, 1000)),
+    },
+    {
+      skip: !minMaxCards,
+    }
+  )
 
   const handleSliderChange = (value: number[]) => {
-    setMinValue(value[0])
-    setMaxValue(value[1])
+    setCardsCount(value)
     setCurrPage(1)
   }
 
   useEffect(() => {
-    setMinValue(minMaxCards?.min)
-    setMaxValue(minMaxCards?.max)
+    if (minMaxCards) {
+      setCardsCount([minMaxCards?.min, minMaxCards?.max])
+    }
   }, [minMaxCards])
 
-  console.log(data?.items)
-
-  if (isLoading) {
+  if (isLoading || cardsCountLoading) {
     return <div style={{ display: 'flex', justifyContent: 'center' }}>LOADING...</div>
   }
 
@@ -74,7 +71,7 @@ export const DecksList = () => {
         </Typography>
 
         <Modal
-          content={<AddNewCard />}
+          content={<AddNewPack />}
           trigger={
             <Button>
               <Typography variant={'subtitle2'}>Add New Deck</Typography>
@@ -89,14 +86,13 @@ export const DecksList = () => {
           onValueChange={setAuthor}
           trigger={[
             { title: 'All Cards', value: '' },
-            { title: 'My Cards', value: `81b2da35-9d0a-42c9-837b-954e9ea9d605` }, // id пока хардкод
+            { title: 'My Cards', value: user?.id ?? '' },
           ]}
         />
         <Slider
-          maxValue={maxValue}
-          minValue={minValue}
+          maxValue={minMaxCards?.max ?? 100}
           onChange={handleSliderChange}
-          value={[minMaxCards?.min ?? 0, minMaxCards?.max ?? 100]}
+          value={cardsCount ?? [0, minMaxCards?.max ?? 100]}
         />
         <Button className={s.clearButton} variant={'secondary'}>
           <Delete />
@@ -104,11 +100,7 @@ export const DecksList = () => {
         </Button>
       </div>
       <div>
-        {author !== '' ? (
-          <DecksTable className={s.tabContent} items={data?.items} myCards />
-        ) : (
-          <DecksTable className={s.tabContent} items={data?.items} />
-        )}
+        {author ? <DecksTable items={data?.items} myCards /> : <DecksTable items={data?.items} />}
       </div>
       <div className={s.paginationBlock}>
         <Pagination
@@ -116,7 +108,6 @@ export const DecksList = () => {
           currentPage={data?.pagination.currentPage}
           onPageChange={setCurrPage}
           pageSize={pageSize}
-          // siblingCount={siblingCount}
           totalCount={data?.pagination.totalItems}
         ></Pagination>
       </div>

bbf96a2e9f30695abcdb4e59490049cd9bf20084
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-05 19:10:52 +0300

    feat: added prop disabled for disable trigger style: add style for disabled trigger

--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -7,14 +7,26 @@ import s from './dropdown.module.scss'
 type DropdownProps = {
   children: ReactNode
   className?: string
+  disabled?: boolean
   onOpenChange?: (open: boolean) => void // для контроля
   open?: boolean // для контроля
   trigger?: ReactNode
 }
-export const Dropdown = ({ children, className, onOpenChange, open, trigger }: DropdownProps) => {
+export const Dropdown = ({
+  children,
+  className,
+  disabled,
+  onOpenChange,
+  open,
+  trigger,
+}: DropdownProps) => {
+  const disable = disabled && s.disabled
+
   return (
     <DropdownMenu.Root onOpenChange={onOpenChange} open={open}>
-      <DropdownMenu.Trigger className={`${s.trigger} ${className}`}>{trigger}</DropdownMenu.Trigger>
+      <DropdownMenu.Trigger className={`${s.trigger} ${className} ${disable}`}>
+        {trigger}
+      </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content className={s.content}>
           <div className={s.triangle}></div>

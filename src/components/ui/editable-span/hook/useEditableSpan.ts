c20dd0feeb32e06c8030325e5c72ec3eb0756eb9
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-07 17:57:04 +0300

    feat: added value & error and handlers for this

--- src/components/ui/editable-span/hook/useEditableSpan.ts
+++ src/components/ui/editable-span/hook/useEditableSpan.ts
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 
 export const useEditableSpan = () => {
+  // value для textfield
+  const [value, setValue] = useState('')
+  // error для textfield
+  const [error, setError] = useState<null | string>(null)
   const [changeOn, setChangeOn] = useState<boolean>(false)
   const toggleMode = () => setChangeOn(prevState => !prevState)
 
-  return { changeOn, setChangeOn, toggleMode } as const
+  return { changeOn, error, setChangeOn, setError, setValue, toggleMode, value } as const
 }

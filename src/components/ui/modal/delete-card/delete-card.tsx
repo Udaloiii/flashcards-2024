2b912eafffac625329ec90228359bd6922033e91
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-07 17:55:40 +0300

    refactor: changed delete-card's title

--- src/components/ui/modal/delete-card/delete-card.tsx
+++ src/components/ui/modal/delete-card/delete-card.tsx
@@ -3,18 +3,13 @@ import { ModalFooter } from '@/components/ui/modal/modal-footer/modal-footer'
 import { ModalHeader } from '@/components/ui/modal/modal-header/modal-header'
 import { Typography } from '@/components/ui/typography'
 
-type DeleteCardProps = {
-  cardTitle?: string
-}
-export const DeleteCard = ({ cardTitle }: DeleteCardProps) => {
+export const DeleteCard = () => {
   return (
     <div>
       <ModalHeader showCloseBtn title={'Delete Card'} />
       <ModalBody>
         <Typography style={{ fontFamily: 'Helvetica, Sans Serif' }} variant={'subtitle1'}>
-          Do you really want to remove {cardTitle}?
-          <br />
-          All cards will be deleted.
+          Do you really want to remove this card?
         </Typography>
       </ModalBody>
       <ModalFooter titlePrimary={'Delete Card'} titleSecondary={'Cancel'} />

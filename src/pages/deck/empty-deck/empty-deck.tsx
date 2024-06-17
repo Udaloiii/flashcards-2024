cb8966dd18431e4f6479cf7d2745963ce963d5f4
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-17 17:32:54 +0300

    feat: wrap AddNewCard in modal; added condition for modal;

--- src/pages/deck/empty-deck/empty-deck.tsx
+++ src/pages/deck/empty-deck/empty-deck.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react'
+
 import EmptyDeckLogo from '@/assets/logo/empty-deck-logo'
 import { Button } from '@/components/ui/button'
+import { Modal } from '@/components/ui/modal'
+import { AddNewCard } from '@/components/ui/modal/add-new-card/add-new-card'
 import { Typography } from '@/components/ui/typography'
 
 import s from './empty-deck.module.scss'
@@ -8,6 +12,8 @@ type EmptyDeckProps = {
   myDeck?: boolean
 }
 export const EmptyDeck = ({ myDeck }: EmptyDeckProps) => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className={s.emptyDeck}>
       <EmptyDeckLogo />
@@ -16,7 +22,9 @@ export const EmptyDeck = ({ myDeck }: EmptyDeckProps) => {
           <Typography className={s.message} variant={'body1'}>
             This pack is empty. Click add new card to fill this pack
           </Typography>
-          <Button>Add New Card</Button>
+          <Modal onOpenChange={setOpen} open={open} trigger={<Button>Add New Card</Button>}>
+            <AddNewCard onOpenChange={setOpen} />
+          </Modal>
         </div>
       ) : (
         <Typography className={s.message} variant={'body1'}>

3ef7fdad1eee11ba62213db1ee163eb2ab0805c6
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-17 17:21:12 +0300

    feat: created baseQueryWithReauth for to prevent multiple calls; feat: replaced fetchBaseQuery in baseQueryWithReauth;

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -1,19 +1,8 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { baseQueryWithReauth } from '@/services/flashCardsBaseQuery'
+import { createApi } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.flashcards.andrii.es',
-    // credentials: 'include',
-    prepareHeaders: headers => {
-      const token = localStorage.getItem('accessToken')
-
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`)
-      }
-
-      return headers
-    },
-  }),
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
   reducerPath: 'baseApi',
   tagTypes: ['Deck', 'Auth', 'Card'],

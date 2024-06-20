448a460794f7ab99af42bc0e87eff5e3e47c14bd
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-20 13:56:51 +0300

    feat: added logic for visibility empty-avatar

--- src/components/ui/dropdown/user-info/user-info.tsx
+++ src/components/ui/dropdown/user-info/user-info.tsx
@@ -9,7 +9,14 @@ type UserInfoProps = {
 export const UserInfo = ({ email, icon, userName }: UserInfoProps) => {
   return (
     <div className={s.container}>
-      <img alt={'user'} className={s.img} src={icon} />
+      {icon ? (
+        <img alt={'user'} className={s.img} src={icon} />
+      ) : (
+        <div className={`${s.img} ${s.defaultAvatar}`}>
+          <Typography variant={'body2'}>{userName?.slice(0, 1)}</Typography>
+        </div>
+      )}
+
       <div className={s.wrap}>
         <Typography variant={'subtitle2'}>{userName}</Typography>
         <Typography className={s.email} variant={'caption'}>

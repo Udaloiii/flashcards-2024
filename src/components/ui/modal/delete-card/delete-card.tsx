e68d5685634a8dc23541a8cf3e6594318d15aa73
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-20 13:52:06 +0300

    feat: created DeleteCard; added logic for this

--- src/components/ui/modal/delete-card/delete-card.tsx
+++ src/components/ui/modal/delete-card/delete-card.tsx
@@ -2,8 +2,19 @@ import { ModalBody } from '@/components/ui/modal/modal-body/modal-body'
 import { ModalFooter } from '@/components/ui/modal/modal-footer/modal-footer'
 import { ModalHeader } from '@/components/ui/modal/modal-header/modal-header'
 import { Typography } from '@/components/ui/typography'
+import { useDeleteCardMutation } from '@/services/cards.service'
 
-export const DeleteCard = () => {
+type DeleteCardProps = {
+  cardId?: string
+  onOpenChange?: (open: boolean) => void
+}
+export const DeleteCard = ({ cardId, onOpenChange }: DeleteCardProps) => {
+  const [deleteCard] = useDeleteCardMutation()
+  const deleteCardHandler = () => {
+    deleteCard({ id: cardId ?? '' })
+    onOpenChange?.(false)
+  }
+
   return (
     <div>
       <ModalHeader showCloseBtn title={'Delete Card'} />
@@ -12,7 +23,11 @@ export const DeleteCard = () => {
           Do you really want to remove this card?
         </Typography>
       </ModalBody>
-      <ModalFooter titlePrimary={'Delete Card'} titleSecondary={'Cancel'} />
+      <ModalFooter
+        onClick={deleteCardHandler}
+        titlePrimary={'Delete Card'}
+        titleSecondary={'Cancel'}
+      />
     </div>
   )
 }

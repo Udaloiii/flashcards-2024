9a221bb4b38b2c09aa3f0822e5a45cb013ea819c
Udaloiii <udaloiiisohappy@gmail.com>
2024-06-21 12:50:33 +0300

    feat: created delete-deck; added logic for this;

--- src/components/ui/modal/delete-deck/delete-deck.tsx
+++ src/components/ui/modal/delete-deck/delete-deck.tsx
@@ -1,23 +1,40 @@
+import { useNavigate } from 'react-router-dom'
+
 import { ModalBody } from '@/components/ui/modal/modal-body/modal-body'
 import { ModalFooter } from '@/components/ui/modal/modal-footer/modal-footer'
 import { ModalHeader } from '@/components/ui/modal/modal-header/modal-header'
 import { Typography } from '@/components/ui/typography'
+import { useDeleteDeckMutation } from '@/services/decks.service'
 
 type DeleteDeckProps = {
+  deckId?: string
   deckTitle?: string
+  onOpenChange?: (open: boolean) => void
 }
-export const DeleteDeck = ({ deckTitle }: DeleteDeckProps) => {
+export const DeleteDeck = ({ deckId = '', deckTitle, onOpenChange }: DeleteDeckProps) => {
+  const [deleteDeck] = useDeleteDeckMutation()
+  const navigate = useNavigate()
+  const deleteDeckHandler = () => {
+    deleteDeck({ id: deckId })
+    onOpenChange?.(false)
+    navigate('/')
+  }
+
   return (
     <div>
-      <ModalHeader showCloseBtn title={'Delete Deck'} />
+      <ModalHeader showCloseBtn title={'Delete'} />
       <ModalBody>
         <Typography style={{ fontFamily: 'Helvetica, Sans Serif' }} variant={'subtitle1'}>
-          Do you really want to remove {deckTitle}?
+          Do you really want to remove{' '}
+          <Typography style={{ textDecoration: 'underline  darkgrey' }} variant={'h3'}>
+            {deckTitle}
+          </Typography>
+          ?
           <br />
           All cards will be deleted.
         </Typography>
       </ModalBody>
-      <ModalFooter titlePrimary={'Delete Card'} titleSecondary={'Cancel'} />
+      <ModalFooter onClick={deleteDeckHandler} titlePrimary={'Delete'} titleSecondary={'Cancel'} />
     </div>
   )
 }

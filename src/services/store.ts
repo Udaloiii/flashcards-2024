22d70936619f9bbe3c22adad72c243c0b494bfe8
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-04 22:10:14 +0300

    feat: added appReducer in store

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,9 +1,11 @@
 import { baseApi } from '@/services/base-api'
+import { appReducer } from '@/store/app-reducer'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
   reducer: {
+    app: appReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
 })

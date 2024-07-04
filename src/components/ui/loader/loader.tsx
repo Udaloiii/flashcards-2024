d3fa28b5f733cbc4881e9772fb47059dc10e41f3
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-04 22:10:40 +0300

    feat: added logic for visible loader

--- src/components/ui/loader/loader.tsx
+++ src/components/ui/loader/loader.tsx
@@ -1,5 +1,11 @@
+import { useSelector } from 'react-redux'
+
+import { RootState } from '@/services/store'
+
 import s from './loader.module.scss'
 
 export const Loader = () => {
-  return <div className={s.loader}></div>
+  const isLoading = useSelector<RootState, boolean>(state => state.app.isLoading)
+
+  return isLoading && <div className={s.loader}></div>
 }

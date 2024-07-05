6a0882efd63ebc8abda71a40c97467284c7169ff
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-05 19:21:05 +0300

    feat: added prop cards for visible condition ('cards' or 'decks')

--- src/pages/decks-list/empty-deck-list/empty-deck-list.tsx
+++ src/pages/decks-list/empty-deck-list/empty-deck-list.tsx
@@ -3,11 +3,16 @@ import { Typography } from '@/components/ui/typography'
 
 import s from './empty-deck-list.module.scss'
 
-export const EmptyDeckList = () => {
+type EmptyDeckListProps = {
+  cards?: boolean
+}
+export const EmptyDeckList = ({ cards }: EmptyDeckListProps) => {
+  const condition = cards ? 'cards' : 'decks'
+
   return (
     <div className={s.container}>
       <Typography className={s.title} variant={'body1'}>
-        Sorry, no decks match the filter
+        Sorry, no {condition} match the filter
       </Typography>
       <EmptyDeckListLogo />
     </div>

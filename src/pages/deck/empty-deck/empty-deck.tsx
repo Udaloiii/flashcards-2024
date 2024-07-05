3580c46f2c260cababe75569633aea7fb49e5f78
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-05 19:23:07 +0300

    refactor: removed Modal in EmptyDeck (just left the add button in Deck)

--- src/pages/deck/empty-deck/empty-deck.tsx
+++ src/pages/deck/empty-deck/empty-deck.tsx
@@ -1,9 +1,4 @@
-import { useState } from 'react'
-
 import EmptyDeckLogo from '@/assets/logo/empty-deck-logo'
-import { Button } from '@/components/ui/button'
-import { Modal } from '@/components/ui/modal'
-import { AddNewCard } from '@/components/ui/modal/add-new-card/add-new-card'
 import { Typography } from '@/components/ui/typography'
 
 import s from './empty-deck.module.scss'
@@ -12,8 +7,6 @@ type EmptyDeckProps = {
   myDeck?: boolean
 }
 export const EmptyDeck = ({ myDeck }: EmptyDeckProps) => {
-  const [open, setOpen] = useState(false)
-
   return (
     <div className={s.emptyDeck}>
       <EmptyDeckLogo />
@@ -22,9 +15,6 @@ export const EmptyDeck = ({ myDeck }: EmptyDeckProps) => {
           <Typography className={s.message} variant={'body1'}>
             This pack is empty. Click add new card to fill this pack
           </Typography>
-          <Modal onOpenChange={setOpen} open={open} trigger={<Button>Add New Card</Button>}>
-            <AddNewCard onOpenChange={setOpen} />
-          </Modal>
         </div>
       ) : (
         <Typography className={s.message} variant={'body1'}>

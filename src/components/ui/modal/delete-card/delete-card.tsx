09d478546e93a59bb23ebc9bcd412c73547d9e2d
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-08 18:45:40 +0300

    feat: added dispatch for setIsLoading & setInfoMessage; added loader for delete-card

--- src/components/ui/modal/delete-card/delete-card.tsx
+++ src/components/ui/modal/delete-card/delete-card.tsx
@@ -1,18 +1,29 @@
+import { useDispatch } from 'react-redux'
+
 import { ModalBody } from '@/components/ui/modal/modal-body/modal-body'
 import { ModalFooter } from '@/components/ui/modal/modal-footer/modal-footer'
 import { ModalHeader } from '@/components/ui/modal/modal-header/modal-header'
 import { Typography } from '@/components/ui/typography'
 import { useDeleteCardMutation } from '@/services/cards.service'
+import { setInfoMessage, setIsLoading } from '@/store/app-reducer'
 
 type DeleteCardProps = {
   cardId?: string
   onOpenChange?: (open: boolean) => void
 }
 export const DeleteCard = ({ cardId, onOpenChange }: DeleteCardProps) => {
-  const [deleteCard] = useDeleteCardMutation()
+  const dispatch = useDispatch()
+  const [deleteCard, otherData] = useDeleteCardMutation()
   const deleteCardHandler = () => {
     deleteCard({ id: cardId ?? '' })
     onOpenChange?.(false)
+    dispatch(setInfoMessage({ message: 'Card deleted' }))
+  }
+
+  if (otherData.isLoading) {
+    dispatch(setIsLoading({ isLoading: true }))
+  } else {
+    dispatch(setIsLoading({ isLoading: false }))
   }
 
   return (

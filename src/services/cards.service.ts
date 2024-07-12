f5f86c6dadbc81ab917ae61341765967acf94ec6
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-12 17:56:26 +0300

    feat: changed invalidatesTags in deleteCard & updateCard (added 'Deck'); added formData in updateCard; fix: fixed incorrect requests (url & method)

--- src/services/cards.service.ts
+++ src/services/cards.service.ts
@@ -5,11 +5,11 @@ export const cardsService = baseApi.injectEndpoints({
   endpoints: build => {
     return {
       deleteCard: build.mutation<void, { id: string }>({
-        invalidatesTags: ['Card'],
+        invalidatesTags: ['Card', 'Deck'],
         query: ({ id }) => {
           return {
             method: 'DELETE',
-            url: `/v1/users/${id}`,
+            url: `/v1/cards/${id}`,
           }
         },
       }),
@@ -22,11 +22,28 @@ export const cardsService = baseApi.injectEndpoints({
         },
       }),
       updateCard: build.mutation<GetCardResponse, { body: UpdateCardRequest; id: string }>({
-        invalidatesTags: ['Card'],
+        invalidatesTags: ['Card', 'Deck'],
         query: arg => {
+          const formData = new FormData()
+
+          if (arg.body.answer) {
+            formData.append('answer', arg.body.answer)
+          }
+          if (arg.body.question) {
+            formData.append('question', arg.body.question)
+          }
+
+          if (arg.body.answerImg) {
+            formData.append('answerImg', arg.body.answerImg)
+          }
+
+          if (arg.body.questionImg) {
+            formData.append('questionImg', arg.body.questionImg)
+          }
+
           return {
-            body: arg.body,
-            method: 'POST',
+            body: formData,
+            method: 'PATCH',
             url: `/v1/cards/${arg.id}`,
           }
         },

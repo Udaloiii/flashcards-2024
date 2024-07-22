cdf2e167a8b31616c065ef947479209847cc80d9
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-22 11:17:30 +0300

    feat: replace all for request in useSelector; added dispatch; added find for question; added isLoading true/false for condition; added a crutch typeof question(null) === 'object' for the primary display of EmptyDeck; fix: changed Modal in correct

--- src/pages/deck/deck.tsx
+++ src/pages/deck/deck.tsx
@@ -1,4 +1,5 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
 import ArrowBack from '@/assets/logo/arrow-back'
@@ -8,41 +9,77 @@ import PlayCircle from '@/assets/logo/play-circle'
 import VerticalDots from '@/assets/logo/vertical-dots'
 import { Button } from '@/components/ui/button'
 import { Dropdown, DropdownItem, DropdownSeparator } from '@/components/ui/dropdown'
+import { Loader } from '@/components/ui/loader/loader'
+import { Modal } from '@/components/ui/modal'
+import { AddNewCard } from '@/components/ui/modal/add-new-card/add-new-card'
+import { ChangeDeck } from '@/components/ui/modal/change-deck/change-deck'
+import { DeleteDeck } from '@/components/ui/modal/delete-deck/delete-deck'
 import { Pagination } from '@/components/ui/pagination/pagination'
 import { CardsTable } from '@/components/ui/table/cards-table/cards-table'
 import { Textfield } from '@/components/ui/textfield'
 import { Typography } from '@/components/ui/typography'
+import { useDebounce } from '@/hooks/useDebounce'
 import { EmptyDeck } from '@/pages/deck/empty-deck/empty-deck'
+import { EmptyDeckList } from '@/pages/decks-list/empty-deck-list/empty-deck-list'
 import { useAuthMeQuery } from '@/services/auth.service'
 import { useGetCardsInDeckQuery, useGetDeckByIdQuery } from '@/services/decks.service'
+import { OrderBySort } from '@/services/flashcards-type'
+import { RootState } from '@/services/store'
+import { setIsLoading } from '@/store/app-reducer'
 
 import s from './deck.module.scss'
 
 export const Deck = forwardRef<ElementRef<'div'>, ComponentPropsWithoutRef<'div'>>(
   ({ className, ...rest }, ref) => {
-    // для пагинации
-    const [currPage, setCurrPage] = useState(1)
-    const [pageSize, setPageSize] = useState(10)
+    const currPage = useSelector<RootState, number>(state => state.cardsList.currPage)
+    const pageSize = useSelector<RootState, number>(state => state.cardsList.pageSize)
+    const dispatch = useDispatch()
+
+    // для поиска по вопросу
+    const [question, setQuestion] = useState<null | string>(null) // добавил null для отображения EmptyDeck
+    // добавил костыль typeof question(null) === 'object' для первичного отображения EmptyDeck
+    // для orderBy
+    // @ts-ignore
+    const [orderBy, setOrderBy] = useState<OrderBySort>(null)
+
+    const [openDropdown, setOpenDropdown] = useState(false)
+    //для модалки на add new card
+    const [open, setOpen] = useState(false)
+    //для модалки на change-deck
+    const [openChange, setOpenChange] = useState(false)
+    //для модалки на delete-deck
+    const [openDelete, setOpenDelete] = useState(false)
 
     const navigate = useNavigate() // для навигации назад
     const { id } = useParams() // чтобы достать id из url
     const { data: deck, isLoading } = useGetDeckByIdQuery({ id: id ?? '' })
     const { data: user } = useAuthMeQuery()
-    const { data } = useGetCardsInDeckQuery({
+    const {
+      data,
+      isFetching,
+      isLoading: isCardsLoading,
+    } = useGetCardsInDeckQuery({
       body: {
         answer: '',
         currentPage: currPage,
         itemsPerPage: pageSize,
-        orderBy: '',
-        question: '',
+        orderBy: orderBy,
+        question: String(useDebounce(question ?? '', 600)),
       },
       id: id ?? '',
     })
+
     const userId = user?.id
     const myCards = userId === deck?.userId // для отображения dropdown
 
-    if (isLoading) {
-      return <div style={{ display: 'flex', justifyContent: 'center' }}>LOADING...</div>
+    if (isCardsLoading || isLoading) {
+      dispatch(setIsLoading({ isLoading: true }))
+
+      return <Loader />
+    } else if (isFetching) {
+      dispatch(setIsLoading({ isLoading: true }))
+    } else {
+      dispatch(setIsLoading({ isLoading: false }))
     }
 
     return (
@@ -55,39 +92,77 @@ export const Deck = forwardRef<ElementRef<'div'>, ComponentPropsWithoutRef<'div'
           <Typography as={'h1'} className={s.title} variant={'h1'}>
             {deck?.name}
             {myCards && (
-              <Dropdown disabled={data?.items.length === 0} trigger={<VerticalDots />}>
-                <Link to={`/${id}/learn`}>
-                  <DropdownItem className={s.dropdownItem}>
+              <Dropdown
+                disabled={data?.items.length === 0}
+                onOpenChange={setOpenDropdown}
+                open={openDropdown}
+                trigger={<VerticalDots />}
+              >
+                <DropdownItem>
+                  <Link className={s.dropdownItem} to={`/${id}/learn`}>
                     <Button disabled={data?.items.length === 0} variant={'icon'}>
                       <PlayCircle />
                     </Button>
                     <Typography variant={'caption'}>Learn</Typography>
-                  </DropdownItem>
-                </Link>
+                  </Link>
+                </DropdownItem>
+
                 <DropdownSeparator />
-                <DropdownItem className={s.dropdownItem}>
-                  <Button variant={'icon'}>
-                    <Edit />
-                    <Typography variant={'caption'}>Edit</Typography>
-                  </Button>
+                <DropdownItem onSelect={() => setOpenChange(true)}>
+                  <Modal
+                    onOpenChange={setOpenChange}
+                    open={openChange}
+                    trigger={
+                      <Button className={s.dropdownItem} variant={'icon'}>
+                        <Edit />
+                        <Typography variant={'caption'}>Edit</Typography>
+                      </Button>
+                    }
+                  >
+                    <ChangeDeck
+                      closeDropdown={setOpenDropdown}
+                      deckId={id ?? ''}
+                      image={deck?.cover}
+                      isPrivate={deck?.isPrivate}
+                      onOpenChange={setOpenChange}
+                      title={deck?.name}
+                    />
+                  </Modal>
                 </DropdownItem>
                 <DropdownSeparator />
-                <DropdownItem className={s.dropdownItem}>
-                  <Button variant={'icon'}>
-                    <Delete />
-                    <Typography variant={'caption'}>Delete</Typography>
-                  </Button>
+                <DropdownItem onSelect={() => setOpenDelete(true)}>
+                  <Modal
+                    onOpenChange={setOpenDelete}
+                    open={openDelete}
+                    trigger={
+                      <Button className={s.dropdownItem} variant={'icon'}>
+                        <Delete />
+                        <Typography variant={'caption'}>Delete</Typography>
+                      </Button>
+                    }
+                  >
+                    <DeleteDeck
+                      deckId={id ?? ''}
+                      deckTitle={deck?.name}
+                      onOpenChange={setOpenDelete}
+                    />
+                  </Modal>
                 </DropdownItem>
               </Dropdown>
             )}
           </Typography>
           {myCards ? (
-            data &&
-            data?.items.length > 0 && (
-              <Button>
-                <Typography variant={'subtitle2'}>Add New Card</Typography>
-              </Button>
-            )
+            <Modal
+              onOpenChange={setOpen}
+              open={open}
+              trigger={
+                <Button>
+                  <Typography variant={'subtitle2'}>Add New Card</Typography>
+                </Button>
+              }
+            >
+              <AddNewCard onOpenChange={setOpen} />
+            </Modal>
           ) : (
             <Button disabled={data?.items.length === 0}>
               <Link to={`/${id}/learn`}>
@@ -97,22 +172,32 @@ export const Deck = forwardRef<ElementRef<'div'>, ComponentPropsWithoutRef<'div'
           )}
         </div>
         {deck?.cover && <img alt={'logo'} className={s.logo} src={deck?.cover} />}
-        <Textfield disabled={data?.items.length === 0} variant={'search'} />
+        <Textfield
+          disabled={typeof question === 'object' && data?.items.length === 0}
+          onChangeValue={setQuestion}
+          placeholder={'search by question '}
+          value={question ?? ''}
+          variant={'search'}
+        />
+        {/* eslint-disable-next-line no-nested-ternary */}
         {data?.items && data?.items.length > 0 ? (
           <>
-            <CardsTable items={data?.items} myDeck={myCards} titleCard={deck?.name} />
+            <CardsTable items={data?.items} myDeck={myCards} />
             <div>
               <Pagination
-                changePageSize={setPageSize}
+                cardsList
                 currentPage={currPage}
-                onPageChange={setCurrPage}
                 pageSize={pageSize}
                 totalCount={data?.pagination.totalItems}
               />
             </div>
           </>
+        ) : question && data?.items && data?.items.length < 1 ? (
+          <div className={s.emptyDeckListBox}>
+            <EmptyDeckList cards />
+          </div>
         ) : (
-          <EmptyDeck myDeck={myCards} />
+          typeof question === 'object' && <EmptyDeck myDeck={myCards} />
         )}
       </div>
     )

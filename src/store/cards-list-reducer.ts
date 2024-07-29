2bef850e6e03e9e7e85151f3f84ad7625e77f327
Udaloiii <udaloiiisohappy@gmail.com>
2024-07-29 18:16:00 +0300

    feat: added sortBy in state; added reducer for this

--- src/store/cards-list-reducer.ts
+++ src/store/cards-list-reducer.ts
@@ -1,18 +1,24 @@
+import { OrderByCardsSort } from '@/services/flashcards-type'
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 type InitialState = {
   currPage: number
   pageSize: number
+  sortBy: OrderByCardsSort
 }
 const slice = createSlice({
   initialState: {
     currPage: 1,
     pageSize: 10,
+    sortBy: 'updated-desc',
   } as InitialState,
   name: 'cards-list',
   reducers: {
     setCurrPageCards: (state, action: PayloadAction<{ currPage: number }>) => {
       state.currPage = action.payload.currPage
+    },
+    setOrderByCards: (state, action: PayloadAction<{ orderBy: OrderByCardsSort }>) => {
+      state.sortBy = action.payload.orderBy
     },
     setPageSizeCards: (state, action: PayloadAction<{ pageSize: number }>) => {
       state.pageSize = action.payload.pageSize
@@ -21,4 +27,4 @@ const slice = createSlice({
 })
 
 export const cardsListReducer = slice.reducer
-export const { setCurrPageCards, setPageSizeCards } = slice.actions
+export const { setCurrPageCards, setOrderByCards, setPageSizeCards } = slice.actions

3d9f8846a655bf03307a56fe6f4415aa88cb6de5
Udaloiii <udaloiiisohappy@gmail.com>
2024-08-17 21:12:19 +0300

    feat: added in props - types for titles; - sortBy; - onClicks for handling sortBy; feat: create onClick depending on the incoming functions; added checkSort for to compare with visionArrow and show the sorting checkbox; create getTitle to display title without asc | desc
    fix: changed sortIconCondition in correct;

--- src/components/ui/table/item-table-head/item-table-head.tsx
+++ src/components/ui/table/item-table-head/item-table-head.tsx
@@ -2,69 +2,113 @@ import ArrowDown from '@/assets/logo/arrow-down'
 import { Button } from '@/components/ui/button'
 import { TableHead, TableRow } from '@/components/ui/table/table'
 import { Typography } from '@/components/ui/typography'
+import { OrderByCardsSort, OrderByDecksSort } from '@/services/flashcards-type'
 
 import s from './item-table-head.module.scss'
 
 type ItemTableHeadProps = {
   className?: string
-  firstTitle?: string
-  fourthTitle?: string
+  firstTitle: OrderByCardsSort | OrderByDecksSort
+  fourthTitle?: OrderByCardsSort | OrderByDecksSort
   myDeck?: boolean
-  onclick?: (value: string) => void
-  secondTitle?: string
-  sortByColumn?: string
-  sortDirection?: 'asc' | 'desc'
-  thirdTitle?: string
+  secondTitle: OrderByCardsSort | OrderByDecksSort
+  sortBy?: OrderByCardsSort | OrderByDecksSort
+  sortCards?: (value: OrderByCardsSort) => void
+  sortDecks?: (value: OrderByDecksSort) => void
+  thirdTitle: OrderByCardsSort | OrderByDecksSort
 }
+
 export const ItemTableHead = ({
   className,
-  firstTitle = '',
-  fourthTitle = '',
+  firstTitle,
+  fourthTitle = null,
   myDeck,
-  onclick,
-  secondTitle = '',
-  sortByColumn,
-  sortDirection,
-  thirdTitle = 'Last Updated',
+  secondTitle,
+  sortBy,
+  sortCards,
+  sortDecks,
+  thirdTitle,
 }: ItemTableHeadProps) => {
-  const sortIconCondition = sortDirection === 'desc' && s.sortUp
+  const sortIconCondition = sortBy?.split('-')[1] === 'asc' && s.sortUp
+
+  const sortLastCell = fourthTitle && s.sortLastCell // для курсора последнего столбца
+
+  const onClick = (value: OrderByCardsSort | OrderByDecksSort) => {
+    if (sortCards) {
+      sortCards(value as OrderByCardsSort) // Утверждение типа для sortCards
+    } else if (sortDecks) {
+      sortDecks(value as OrderByDecksSort) // Утверждение типа для sortDecks
+    }
+  }
+
+  const visionArrow = sortBy?.split('-')[0]
+  const checkSort = (title: OrderByCardsSort | OrderByDecksSort) => title?.split('-')[0] // для сравнения с visionArrow и показывания галочки ортировки
+  const getTitle = (title: null | string) => {
+    switch (title) {
+      case 'cardsCount-desc':
+        title = 'cards'
+        break
+      case 'updated-desc':
+        title = 'last updated'
+        break
+      case 'created-desc':
+        title = 'created by'
+        break
+      default:
+        break
+    }
+    let newTitle = title?.split('-')[0] ?? ''
+    const firstChar = newTitle.slice(0, 1)[0].toUpperCase()
+
+    newTitle = firstChar + newTitle.slice(1)
+
+    return newTitle
+  } // для отображения title без asc | desc
 
   return (
     <TableRow className={`${s.container} ${className}`}>
-      <TableHead className={s.tableHead} onClick={() => onclick?.(firstTitle)}>
-        <Typography variant={'subtitle2'}>{firstTitle}</Typography>
-        {sortByColumn === firstTitle && (
-          <Button
-            className={`${s.sort} ${sortIconCondition}`}
-            onSort={() => onclick?.('Name')}
-            variant={'icon'}
-          >
+      <TableHead className={s.tableHead} onClick={() => onClick(firstTitle)}>
+        <Typography variant={'subtitle2'}>{getTitle(firstTitle)}</Typography>
+        {visionArrow === checkSort(firstTitle) && (
+          <Button className={`${s.sort} ${sortIconCondition}`} variant={'icon'}>
             <ArrowDown />
           </Button>
         )}
       </TableHead>
-      <TableHead className={s.tableHead} onClick={() => onclick?.(secondTitle)}>
+      <TableHead className={s.tableHead} onClick={() => onClick(secondTitle)}>
         <Typography variant={'subtitle2'}>
-          {secondTitle}
-          {sortByColumn === secondTitle && (
+          {getTitle(secondTitle)}
+          {visionArrow === checkSort(secondTitle) && (
             <Button className={`${s.sort} ${sortIconCondition}`} variant={'icon'}>
               <ArrowDown />
             </Button>
           )}
         </Typography>
       </TableHead>
-      <TableHead className={s.tableHead} onClick={() => onclick?.(thirdTitle)}>
+      <TableHead className={s.tableHead} onClick={() => onClick(thirdTitle)}>
         <Typography variant={'subtitle2'}>
-          {thirdTitle}
-          {sortByColumn === thirdTitle && (
+          {getTitle(thirdTitle)}
+          {visionArrow === checkSort(thirdTitle) && (
             <Button className={`${s.sort} ${sortIconCondition}`} variant={'icon'}>
               <ArrowDown />
             </Button>
           )}
         </Typography>
       </TableHead>
-      <TableHead className={s.tableHead}>
-        <Typography variant={'subtitle2'}>{fourthTitle}</Typography>
+      <TableHead
+        className={`${s.tableHead} ${s.sortLastCell}`}
+        onClick={() => onClick(fourthTitle)}
+      >
+        {fourthTitle && (
+          <Typography variant={'subtitle2'}>
+            {getTitle(fourthTitle)}
+            {visionArrow === checkSort(fourthTitle) && (
+              <Button className={`${s.sort} ${sortIconCondition} ${sortLastCell}`} variant={'icon'}>
+                <ArrowDown />
+              </Button>
+            )}
+          </Typography>
+        )}
       </TableHead>
       {myDeck && <TableHead className={s.tableHead} />}
     </TableRow>
